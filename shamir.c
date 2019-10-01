34521cecfb66eb75c4001cedded72b6004421486
ShamirRobayo69 <55764768+ShamirRobayo69@users.noreply.github.com>
2019-10-01 14:29:14 -0500

    Add files via upload

---
+++ shamir.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#define pi 3.1416
+void main()
+{
+	float radio,area=0;
+	printf("Ingrese el radio:");
+	scanf("%f",&radio);
+	area=pi*pow(radio,2);
+	printf("El area es:%.2f",area);
+}
